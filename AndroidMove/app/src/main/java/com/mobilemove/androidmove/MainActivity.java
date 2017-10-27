0f1f5265ec6a894b003538b06eb621414291996e
guilschaidhauer <guilherme.schaidhauer.de.castro@sap.com>
2017-10-26 23:20:22 -0200

    Add phone rotation

--- AndroidMove/app/src/main/java/com/mobilemove/androidmove/MainActivity.java
+++ AndroidMove/app/src/main/java/com/mobilemove/androidmove/MainActivity.java
@@ -4,10 +4,12 @@ import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorEvent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.graphics.Color;
 import android.util.Log;
+import android.view.View;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
@@ -17,17 +19,21 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     SensorManager sensorManager;
     Sensor rotationVectorSensor;
 
+    private View root=null;
+
     float[] orientations;
+    float[] gyro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        root=findViewById(android.R.id.content);
 
         sensorManager =
                 (SensorManager) getSystemService(SENSOR_SERVICE);
@@ -40,7 +46,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSensorChanged(SensorEvent sensorEvent) {
                 //getWindow().getDecorView().setBackgroundColor(Color.BLUE);
-
+                gyro = new float[4];
+                gyro = sensorEvent.values;
                 float[] rotationMatrix = new float[16];
                 SensorManager.getRotationMatrixFromVector(
                         rotationMatrix, sensorEvent.values);
@@ -84,15 +91,27 @@ public class MainActivity extends AppCompatActivity {
     protected void sendMessage() {
         try {
             //String messageStr = "Hello World";
-            Integer[] orInt = new Integer[3];
+            Integer[] orInt = new Integer[4];
 
-            orInt[0] = (int)orientations[0];
-            orInt[1] = (int)orientations[1];
-            orInt[2] = (int)orientations[2];
+            //orInt[0] = (int)orientations[0];
+            //orInt[1] = (int)orientations[1];
+            //orInt[2] = (int)orientations[2];
+
+            orInt[0] = (int)(gyro[0] * 1000000);
+            orInt[1] = (int)(gyro[1] * 1000000);
+            orInt[2] = (int)(gyro[2] * 1000000);
+            orInt[3] = (int)(gyro[3] * 1000000);
 
             String messageStr = Integer.toString(orInt[0]) + "|";
             messageStr += Integer.toString(orInt[1]) + "|";
-            messageStr += Integer.toString(orInt[2]);
+            messageStr += Integer.toString(orInt[2]) + "|";
+            messageStr += Integer.toString(orInt[3]);
+
+            /*String messageStr = Float.toString(gyro[0]) + "|";
+            messageStr += Float.toString(gyro[1]) + "|";
+            messageStr += Float.toString(gyro[2]) + "|";
+            messageStr += Float.toString(gyro[3]);*/
+
             int server_port = 7777;
             InetAddress local = InetAddress.getByName("192.168.2.193");
             int msg_length = messageStr.length();
